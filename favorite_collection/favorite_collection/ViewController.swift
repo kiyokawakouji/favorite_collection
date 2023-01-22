2f276abf1d4b1bd9a506afad2c3de7e90bb5da37
kiyokawakouji <kohji12k@gmail.com>
2023-01-22 23:43:13 +0900

    hello,world

---
+++ favorite_collection/favorite_collection/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  favorite_collection
+//
+//  Created by 清川光司 on R 5/01/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
